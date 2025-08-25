9239e02bfd542dd9564ce10caf0d440ca32ca8e6
iNiloowf <niloowf@gmail.com>
2025-08-25 17:25:45 -0600

    create basic navbar component

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
-
+import React from 'react';
+import './Navbar.css'
+import { Link } from 'react-router-dom';
+import { FaBitcoin } from "react-icons/fa";
 const NavbarComponent = () => {
   return (
-   <div>Navbar</div>
+   <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+     <Link class="navbar-brand" href="#">
+     <FaBitcoin className="navbar-logo"/>
+     Crypto Tracking
+     </Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+           <li class="nav-item">
+          <Link class="nav-link" to="/">Home</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/faq">FAQ</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/aboutus">About Us</Link>
+        </li>
+      <li class="nav-item">
+          <Link class="nav-link" to="/contactus">Contact Us</Link>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
   );
 };
 
