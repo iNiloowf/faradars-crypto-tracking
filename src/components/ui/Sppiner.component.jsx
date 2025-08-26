b432fafeee42ffc3418d036282369784a3b3d911
iNiloowf <niloowf@gmail.com>
2025-08-26 14:29:30 -0600

    add spinner component

---
+++ src/components/ui/Sppiner.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SppinerImg from '../../assets/images/sppiner.gif'
+
+const SppinerComponent = () => {
+  return (
+    <div className="d-flex justify-content-center align-item-center">
+        <img src={SppinerImg} alt="Sppiner" />
+     </div>
+  );
+};
+
+export default SppinerComponent;
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import SppinerComponent from '../components/ui/Sppiner.component';
 
 const HomePage = () => {
   return (
+    <div>
     <div>Home.page</div>
+    <SppinerComponent />
+    </div>
   );
 };
 
