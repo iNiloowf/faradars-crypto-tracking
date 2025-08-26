416bc18bd106dc6b9856ef94d07941ce100fb5a1
iNiloowf <niloowf@gmail.com>
2025-08-26 15:21:55 -0600

    add about us page content

--- src/pages/Aboutus.page.jsx
+++ src/pages/Aboutus.page.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import aboutusImg from "../assets/images/aboutus.png"
 const AboutusPage = () => {
   return (
-    <div>Aboutus.page</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded">
+          <h3>About us</h3>
+        </div>
+      </div>
+      <div className="row align-item-center p-2">
+        <div className="col-md-6">
+          <img src={aboutusImg} alt="About us" className="img-fluid" />
+        </div>
+        <div className="col-md-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.dolor sit amet consectetur adipisicing elit.dolor sit amet consectetur adipisicing elit.dolor sit amet consectetur adipisicing elit.dolor sit amet consectetur adipisicing elit. Ipsa eligendi officiis dolorem quod qui eveniet repudiandae repellendus, nostrum dolore, perferendis quia magnam error natus modi commodi unde! Eligendi, facere odit laboriosam consequuntur error ipsa natus debitis voluptatibus quisquam quo, dolore deleniti cupiditate, voluptas nemo harum eum deserunt quod ullam officia commodi voluptatem? Placeat odio quaerat quo vero quam labore debitis nihil excepturi dolores officia fugiat maxime odit velit explicabo sit qui ratione impedit, beatae incidunt vitae possimus ipsam sed maiores. Rerum blanditiis deleniti facere inventore reiciendis? Sint, ex voluptas provident aspernatur, fugiat natus rem architecto fugit quo in illum harum!
+        </div>
+      </div>
+      </div>
   )
 }
 
